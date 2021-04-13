a7b68985025fbb125e57775ebed9ec55eec3c381
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-13 22:54:29 +0100

    Create LoanSoapConfig.java

---
+++ src/main/java/de/tekup/loan/config/LoanSoapConfig.java
@@ -0,0 +1,44 @@
+package de.tekup.loan.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+import de.tekup.loan.endpoint.LoanEligebiltyEndPoint;
+
+@EnableWs
+@Configuration
+public class LoanSoapConfig {
+	
+	@Bean
+	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context){
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		
+		servlet.setApplicationContext(context);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<MessageDispatcherServlet>(servlet,"/ws/*");
+	}
+	
+	@Bean
+	public XsdSchema schema() {
+		return new SimpleXsdSchema(new ClassPathResource("loanEligebilty.xsd"));
+	}
+	
+	@Bean(name = "loanEligebilty")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema schema) {
+		DefaultWsdl11Definition defaultWsdl11Definition = new DefaultWsdl11Definition();
+		defaultWsdl11Definition.setPortTypeName("LoanEligebiltyIndicator");
+		defaultWsdl11Definition.setLocationUri("/ws");
+		defaultWsdl11Definition.setSchema(schema);
+		defaultWsdl11Definition.setTargetNamespace(LoanEligebiltyEndPoint.nameSpace);
+		return defaultWsdl11Definition;
+	}
+
+}
