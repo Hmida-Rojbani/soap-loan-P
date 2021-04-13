440fecc0857f1a11dc47fb985633f0a18b33d7c1
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-13 22:38:00 +0100

    Create LoanEligebiltyService.java

---
+++ src/main/java/de/tekup/loan/service/LoanEligebiltyService.java
@@ -0,0 +1,40 @@
+package de.tekup.loan.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import de.tekup.loan.soap.ws.loaneligebilty.CustomerRequest;
+import de.tekup.loan.soap.ws.loaneligebilty.ObjectFactory;
+import de.tekup.loan.soap.ws.loaneligebilty.WsResponse;
+
+@Service
+public class LoanEligebiltyService {
+
+	public WsResponse checkLoanEligebilty(CustomerRequest customerRequest) {
+		WsResponse wsResponse = new ObjectFactory().createWsResponse();
+		
+		List<String> mismatchs = wsResponse.getCriteriaMismatch();
+		
+		if(!(customerRequest.getAge() >= 30 && customerRequest.getAge() <= 50)) {
+			mismatchs.add("Client age must be between  30 and 50.");
+		}
+		if(!(customerRequest.getYearlyIncome() >= 25000)) {
+			mismatchs.add("Client yearly income must be over 25000.");
+		}
+		if(!(customerRequest.getCibilScore().intValue() >= 500)) {
+			mismatchs.add("Client cibil score must be over 500.");
+		}
+		
+		if (mismatchs.isEmpty()) {
+			wsResponse.setIsEligeble(true);
+			long amount = (long)((60 - customerRequest.getAge())*customerRequest.getYearlyIncome()*0.4);
+			wsResponse.setApprovedAmount(amount);
+		}else {
+			wsResponse.setIsEligeble(false);
+			wsResponse.setApprovedAmount(0);
+		}
+		
+		return null;
+	}
+}
