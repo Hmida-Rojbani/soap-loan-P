8e3ec39c429f993cb952ea2be4e07ce3f817400e
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-13 21:21:08 +0100

    soap step 1

---
+++ src/main/java/de/tekup/loan/SoapLoanPApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapLoanPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapLoanPApplication.class, args);
+	}
+
+}
