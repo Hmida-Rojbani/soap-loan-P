8a07367dac213128c3ba570351576471914c6f80
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-13 22:54:23 +0100

    Update LoanEligebiltyEndPoint.java

--- src/main/java/de/tekup/loan/endpoint/LoanEligebiltyEndPoint.java
+++ src/main/java/de/tekup/loan/endpoint/LoanEligebiltyEndPoint.java
@@ -1,10 +1,12 @@
 package de.tekup.loan.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import de.tekup.loan.service.LoanEligebiltyService;
 import de.tekup.loan.soap.ws.loaneligebilty.CustomerRequest;
 import de.tekup.loan.soap.ws.loaneligebilty.WsResponse;
 
@@ -12,10 +14,12 @@ import de.tekup.loan.soap.ws.loaneligebilty.WsResponse;
 public class LoanEligebiltyEndPoint {
 
 	public static final String nameSpace="http://www.tekup.de/loan/soap/ws/loanEligebilty";
+	@Autowired
+	private LoanEligebiltyService service;
 	
-	@PayloadRoot(namespace = nameSpace,localPart = "CustomerRequest")
+	@PayloadRoot(namespace = nameSpace, localPart = "CustomerRequest")
 	@ResponsePayload
 	public WsResponse getLoanStatus(@RequestPayload CustomerRequest customerRequest) {
-		return null;
+		return service.checkLoanEligebilty(customerRequest);
 	}
 }
